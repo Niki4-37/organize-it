3f1d8f7e1257039e4d909dad65de6da44e14c61c
Artem Nikonov <ArTeMnikonov2807@gmail.com>
2025-10-12 16:21:04 +0300

    UtilArray now work with Generics

--- organize-it/src/main/java/com/secondteam/utils/UtilArray.java
+++ organize-it/src/main/java/com/secondteam/utils/UtilArray.java
@@ -1,40 +1,38 @@
 package com.secondteam.utils;
 
-import com.secondteam.person.Person;
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class UtilArray {
+public class UtilArray<E> {
     private static final int PARALLEL_THRESHOLD = 1000;
 
-    private static class MergeSortTask extends RecursiveTask<ArrayList<Person>> {
-        private final ArrayList<Person> array;
-        private final Comparator<Person> comparator;
+    private class MergeSortTask extends RecursiveTask<ArrayList<E>> {
+        private final ArrayList<E> array;
+        private final Comparator<E> comparator;
 
         @Override
-        protected ArrayList<Person> compute() {
+        protected ArrayList<E> compute() {
             if (array.size() == 1) {
                 return array;
             }
             int mid = array.size() / 2;
             if(array.size() < PARALLEL_THRESHOLD) {
-                ArrayList<Person> left = mergeSort(new ArrayList<>(array.subList(0, mid)), comparator);
-                ArrayList<Person> right = mergeSort(new ArrayList<>(array.subList(mid, array.size())), comparator);
+                ArrayList<E> left = mergeSort(new ArrayList<>(array.subList(0, mid)), comparator);
+                ArrayList<E> right = mergeSort(new ArrayList<>(array.subList(mid, array.size())), comparator);
                 return merge(left, right, comparator);
             }
             MergeSortTask leftTask = new MergeSortTask(new ArrayList<>(array.subList(0, mid)), comparator);
             leftTask.fork();
             MergeSortTask rightTask = new MergeSortTask(new ArrayList<>(array.subList(mid, array.size())), comparator);
-            ArrayList<Person> right = rightTask.compute();
-            ArrayList<Person> left = leftTask.join();
+            ArrayList<E> right = rightTask.compute();
+            ArrayList<E> left = leftTask.join();
             return merge(left, right, comparator);
         }
 
-        private static ArrayList<Person> merge(ArrayList<Person> left, ArrayList<Person> right, Comparator<Person> comparator) {
-            ArrayList<Person> result = new ArrayList<>(left.size() + right.size());
+        private ArrayList<E> merge(ArrayList<E> left, ArrayList<E> right, Comparator<E> comparator) {
+            ArrayList<E> result = new ArrayList<>(left.size() + right.size());
             int leftIndex = 0, rightIndex = 0;
             while (leftIndex < left.size() && rightIndex < right.size()) {
                 if (comparator.compare(left.get(leftIndex), right.get(rightIndex)) > 0) {
@@ -53,36 +51,33 @@ public class UtilArray {
             return result;
         }
 
-        public MergeSortTask(ArrayList<Person> array, Comparator<Person> comparator) {
+        public MergeSortTask(ArrayList<E> array, Comparator<E> comparator) {
             this.array = array;
             this.comparator = comparator;
         }
     }
 
-    private ArrayList<Person> array;
+    private ArrayList<E> array;
 
-    public void sort() {
-        sort(Comparator.comparing(Person::getAge));
-    }
 
-    public void sort(Comparator<Person> comparator) {
+    public void sort(Comparator<E> comparator) {
         array = mergeSort(array, comparator);
     }
 
-    private static ArrayList<Person> mergeSort(ArrayList<Person> array, Comparator<Person> comparator) {
+    private ArrayList<E> mergeSort(ArrayList<E> array, Comparator<E> comparator) {
         ForkJoinPool pool = ForkJoinPool.commonPool();
         return pool.invoke(new MergeSortTask(array, comparator));
     }
 
-    public UtilArray(ArrayList<Person> array) {
+    public UtilArray(ArrayList<E> array) {
         this.array = array;
     }
 
-    public ArrayList<Person> getArray() {
+    public ArrayList<E> getArray() {
         return array;
     }
 
-    public void setArray(ArrayList<Person> array) {
+    public void setArray(ArrayList<E> array) {
         this.array = array;
     }
 }
