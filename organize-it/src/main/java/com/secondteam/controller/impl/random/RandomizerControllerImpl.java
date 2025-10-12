beca51669e24c0b2a4e55522f69abb1cbab3bfc1
Darya <dashatsvetkov@gmail.com>
2025-10-12 15:53:09 +0300

    Написала реализацию для метода execute через stream. убрала параметры из конструктора.

--- organize-it/src/main/java/com/secondteam/controller/impl/random/RandomizerControllerImpl.java
+++ organize-it/src/main/java/com/secondteam/controller/impl/random/RandomizerControllerImpl.java
@@ -2,25 +2,33 @@ package com.secondteam.controller.impl.random;
 
 import com.secondteam.controller.Controller;
 import com.secondteam.exception.AppException;
+import com.secondteam.utils.DelegateListener;
+import com.secondteam.utils.UtilArray;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 public class RandomizerControllerImpl implements Controller {
-    private final EntitySynthFactory;
+    private final EntitySynthFactory generator = new EntitySynthFactory();
     private final Random random = new Random();
 
-    public RandomizerControllerImpl(Randomize<T> randomize) {
-        this.generator = randomize;
-    }
-
-    @Override
-    public List<T> execute() throws AppException {
-        var list = new ArrayList<T>();
-        for (int i = 0; i < random.nextInt(100); i++) {
-            list.add(generator.random());
+    public RandomizerControllerImpl() {
         }
-        return list;
-    }
-}
+
+        @Override
+        public void execute(DelegateListener delegate) {
+
+            UtilArray entities = Optional.of(random.ints()
+                .limit(10)
+                .mapToObj(i -> generator.random())
+                .toList())
+                .map(ArrayList::new)
+                .map(UtilArray::new)
+                .get();
+
+                delegate.executionCompleted();
+            }
+        }
+
