bd33ba8502479191ae99d07f0167e262816c68e2
Darya <dashatsvetkov@gmail.com>
2025-10-12 16:30:13 +0300

    revert 2

---
+++ organize-it/src/main/java/com/secondteam/controller/impl/random/RandomizerControllerImpl.java
@@ -0,0 +1,32 @@
+package com.secondteam.controller.impl.random;
+
+import com.secondteam.controller.Controller;
+import com.secondteam.utils.DelegateListener;
+import com.secondteam.utils.UtilArray;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Random;
+
+public class RandomizerControllerImpl implements Controller {
+    private final EntitySynthFactory generator = new EntitySynthFactory();
+    private final Random random = new Random();
+
+    public RandomizerControllerImpl() {
+    }
+
+    @Override
+    public void execute(DelegateListener delegate) {
+
+        UtilArray entities = Optional.of(random.ints()
+                        .limit(10)
+                        .mapToObj(i -> generator.random())
+                        .toList())
+                .map(ArrayList::new)
+                .map(UtilArray::new)
+                .get();
+
+        delegate.executionCompleted();
+    }
+}
+
