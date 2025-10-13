2668fa0a7957b07f1c5911383ba6858f1bcfdd52
Darya <dashatsvetkov@gmail.com>
2025-10-13 19:38:22 +0300

    Custom list size, modifiable list

--- organize-it/src/main/java/com/secondteam/controller/random/RandomizerControllerImpl.java
+++ organize-it/src/main/java/com/secondteam/controller/random/RandomizerControllerImpl.java
@@ -4,19 +4,34 @@ import com.secondteam.controller.Controller;
 import com.secondteam.exception.AppException;
 import com.secondteam.person.Person;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class RandomizerControllerImpl implements Controller<Person> {
     private final EntitySynthFactory generator = new EntitySynthFactory();
     private final Random random = new Random();
+    private final int size;
+
+    public RandomizerControllerImpl() {
+        this.size = random.nextInt(0, 100);
+    }
+
+    public RandomizerControllerImpl(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("Count must be positive: " + size);
+        }
+        this.size = size;
+    }
+
 
     @Override
     public List<Person> execute() throws AppException {
         return random.ints()
-                        .limit(10)
-                        .mapToObj(i -> generator.random())
-                        .toList();
+                .limit(size)
+                .mapToObj(i -> generator.random())
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 }
 
