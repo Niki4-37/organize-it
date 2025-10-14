28ab84339f5ccc9d58ba3c0273b0c9f53e630c0f
Darya <dashatsvetkov@gmail.com>
2025-10-14 17:18:43 +0300

    add validate, moved the sizing to the stream

--- organize-it/src/main/java/com/secondteam/controller/random/RandomizerControllerImpl.java
+++ organize-it/src/main/java/com/secondteam/controller/random/RandomizerControllerImpl.java
@@ -1,5 +1,6 @@
 package com.secondteam.controller.random;
 
+import com.secondteam.consolehandler.ConsoleHandler;
 import com.secondteam.controller.Controller;
 import com.secondteam.exception.AppException;
 import com.secondteam.person.Person;
@@ -12,26 +13,25 @@ import java.util.stream.Collectors;
 public class RandomizerControllerImpl implements Controller<Person> {
     private final EntitySynthFactory generator = new EntitySynthFactory();
     private final Random random = new Random();
-    private final int size;
-
-    public RandomizerControllerImpl() {
-        this.size = random.nextInt(0, 100);
-    }
-
-    public RandomizerControllerImpl(int size) {
-        if (size <= 0) {
-            throw new IllegalArgumentException("Count must be positive: " + size);
-        }
-        this.size = size;
-    }
-
 
     @Override
     public List<Person> execute() throws AppException {
+        ConsoleHandler.write("Сколько людей вы хотите добавить? Введите положительное целое число:");
         return random.ints()
-                .limit(size)
+                .limit(validateInput(ConsoleHandler.read()))
                 .mapToObj(i -> generator.random())
                 .collect(Collectors.toCollection(ArrayList::new));
     }
-}
 
+    private int validateInput(String input) throws AppException {
+        try {
+            int value = Integer.parseInt(input);
+            if (value < 0) {
+                throw new AppException("Указанное число меньше 0");
+            }
+            return value;
+        } catch (NumberFormatException e) {
+            throw new AppException(e.getMessage(), e);
+        }
+    }
+}
